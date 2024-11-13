501af5d645cece5e0678e2d8102edd98044dfacc
Idoia Bergera <idoia.bergera@opendeusto.es>
2024-11-13 18:34:21 +0100

    Prueba

--- src/main/java/Strava/dto/UsuarioDTO.java
+++ src/main/java/Strava/dto/UsuarioDTO.java
@@ -1,5 +1,54 @@
 package Strava.dto;
 
 public class UsuarioDTO {
+	private String email;
+	private String nombre;
+	private String fechaNacimiento;
+	private int peso;
+	private int altura;
+	private int FCmax_ppm;
+	private int FCrep_ppm;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+	public void setFechaNacimiento(String fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	public int getPeso() {
+		return peso;
+	}
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+	public int getAltura() {
+		return altura;
+	}
+	public void setAltura(int altura) {
+		this.altura = altura;
+	}
+	public int getFCmax_ppm() {
+		return FCmax_ppm;
+	}
+	public void setFCmax_ppm(int fCmax_ppm) {
+		FCmax_ppm = fCmax_ppm;
+	}
+	public int getFCrep_ppm() {
+		return FCrep_ppm;
+	}
+	public void setFCrep_ppm(int fCrep_ppm) {
+		FCrep_ppm = fCrep_ppm;
+	}
 
 }
