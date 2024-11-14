ec0b41aaefd75ebd0ebd0be2388309893241bc81
Irene Montserrat <irene.montserrat@opendeusto.es>
2024-11-14 19:46:35 +0100

    Resolucion de errores

--- src/main/java/Strava/StravaApplication.java
+++ src/main/java/Strava/StravaApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class StravaApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(StravaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StravaApplication.class, args);
+    }
 }
