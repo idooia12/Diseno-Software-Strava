f77dc75393759709b2504bb0455b083c42dbbc04
Jon Ander Areizaga <jonander.areizaga@opendeusto.es>
2024-12-03 11:36:06 +0100

    pruebas con implementación de Google

--- src/main/java/Strava/gateway/ServiceGateway.java
+++ src/main/java/Strava/gateway/ServiceGateway.java
@@ -8,21 +8,21 @@ import org.springframework.stereotype.Component;
 public class ServiceGateway {
 
     private final MetaGateway metaServiceGateway;
- //   private final GoogleGateway googleServiceGateway;
+    private final GoogleGateway googleServiceGateway;
     private final AuthorizationService authorizationService;
 
     public ServiceGateway(MetaGateway metaServiceGateway,
     					  GoogleGateway googleServiceGateway,
                           AuthorizationService authorizationService) {
         this.metaServiceGateway = metaServiceGateway;
- //       this.googleServiceGateway = googleServiceGateway;
+        this.googleServiceGateway = googleServiceGateway;
         this.authorizationService = authorizationService;
     }
     
 
     public boolean validar(String email, String key) {
         return switch (key) {
-         //   case "GOOGLE" -> googleServiceGateway.validar(email);
+        	case "GOOGLE" -> googleServiceGateway.validar(email);
             case "META" -> metaServiceGateway.validar(email);
             default -> throw new IllegalArgumentException("Unexpected service key: " + key);
         };
@@ -30,7 +30,7 @@ public class ServiceGateway {
 
     public boolean login(String email, String password, String key) {
         boolean isAuthenticated = switch (key) {
-           // case "GOOGLE" -> googleServiceGateway.login(email, password);
+           	case "GOOGLE" -> googleServiceGateway.login(email, password);
             case "META" -> metaServiceGateway.login(email, password);
             default -> throw new IllegalArgumentException("Unexpected service key: " + key);
         };
@@ -45,7 +45,7 @@ public class ServiceGateway {
 
     public void setInstance(String servIP, int servPort, String key) {
         switch (key) {
-        //    case "GOOGLE" -> googleServiceGateway.setInstance(servIP, servPort);
+        	case "GOOGLE" -> googleServiceGateway.setInstance(servIP, servPort);
             case "META" -> metaServiceGateway.setInstance(servIP, servPort);
             default -> throw new IllegalArgumentException("Unexpected service key: " + key);
         }
@@ -53,13 +53,9 @@ public class ServiceGateway {
     
     public UsuarioEntity getUsuarioByEmail(String email, String key) {
         return switch (key) {
-            case "META" -> metaServiceGateway.getUsuarioByEmail(email, key);
+        	case "GOOGLE" -> googleServiceGateway.getUsuarioByEmail(email, key);
+        	case "META" -> metaServiceGateway.getUsuarioByEmail(email, key);
             default -> throw new IllegalArgumentException("Unexpected service key: " + key);
         };
-    
     }
-    
-
-
-
 }
