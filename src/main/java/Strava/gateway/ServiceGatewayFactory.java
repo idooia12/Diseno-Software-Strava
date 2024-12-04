32e013c50c7cb249abc0629678538a6ce70bdedb
ManuPrudencio <manu.prudencio@opendeusto.es>
2024-12-04 13:36:03 +0100

    getInstance factory

--- src/main/java/Strava/gateway/ServiceGatewayFactory.java
+++ src/main/java/Strava/gateway/ServiceGatewayFactory.java
@@ -2,18 +2,18 @@ package Strava.gateway;
 
 import jakarta.inject.Singleton;
 import Strava.entity.ServicioValidacion;
-import Strava.entity.ServicioValidacionEnum;
-
 
 @Singleton
 public class ServiceGatewayFactory {
 
+    // Instancia única (Singleton)
     private static ServiceGatewayFactory instance;
 
+    // Constructor privado para evitar la creación directa de instancias
     private ServiceGatewayFactory() {
-        // Constructor privado para asegurar Singleton
     }
 
+    
     public static synchronized ServiceGatewayFactory getInstance() {
         if (instance == null) {
             instance = new ServiceGatewayFactory();
@@ -24,12 +24,11 @@ public class ServiceGatewayFactory {
     public ServiceGatewayInterface crearGateway(ServicioValidacion tipo) {
         switch (tipo) {
             case Google:
-                return new GoogleGateway();
+                return new GoogleGateway(); // Asegúrate de que esta clase implemente ServiceGatewayInterface
             case Meta:
-                return new MetaGateway();
+                return new MetaGateway(); // Asegúrate de que esta clase implemente ServiceGatewayInterface
             default:
                 throw new IllegalArgumentException("Tipo de servicio no soportado: " + tipo);
         }
     }
 }
-
