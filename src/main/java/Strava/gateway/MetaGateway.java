602902e036c16f275e16836ea74630f3b780e8ba
Irene Montserrat <irene.montserrat@opendeusto.es>
2024-12-05 18:05:52 +0100

    Soluciones al Gateway, Meta ya funciona

--- src/main/java/Strava/gateway/MetaGateway.java
+++ src/main/java/Strava/gateway/MetaGateway.java
@@ -61,7 +61,6 @@ public class MetaGateway implements ServiceGatewayInterface  {
      */
     public boolean login(String email, String password) {
         String mensaje = "validar" + DELIMITER + email + DELIMITER + password;
-        UsuarioEntity usuarioEntity = null;
         boolean resultado = false;
 
         try (Socket socket = new Socket(serverIP, serverPort);
@@ -75,22 +74,18 @@ public class MetaGateway implements ServiceGatewayInterface  {
             // Recibir respuesta del servidor
             String respuesta = in.readUTF();
             System.out.println(" - Response from Meta server: " + respuesta);
+            System.out.println(respuesta);
 
             // Procesar la respuesta del servidor para construir un UsuarioEntity
-            if (respuesta != null && !respuesta.equals("null")) {
-                String[] partes = respuesta.split(DELIMITER);
-                if (partes.length >= 2) {
-                    usuarioEntity = new UsuarioEntity();
-                    usuarioEntity.setEmail(partes[0]);  // Primer elemento es el email
-                    usuarioEntity.setContraseña(partes[1]);  // Segundo elemento es la contraseña
-                    // Agregar otros campos necesarios aqu
-                    resultado = true;
-                }
+            if (respuesta.equals("true")) {
+            	resultado = true;
+            	System.out.println("No entra aqui?");
 			} 
         } catch (Exception e) {
             System.err.println("# MetaGateway error in login: " + e.getMessage());
         }
         return resultado;
+        
 
         //return usuarioEntity; // Devuelve el usuario o null si falló
     }
